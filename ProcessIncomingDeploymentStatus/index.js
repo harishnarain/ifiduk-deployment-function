cfe6a42035eaf8fcdddde59ed021e23eb37e3da6
Harish Narain <harishnarain@gmail.com>
2021-02-07 21:01:58 +1100

    Added ProcessIncomingDeploymentStatus function. Validated functional.

---
+++ ProcessIncomingDeploymentStatus/index.js
@@ -0,0 +1,20 @@
+const db = require('../config/dbConfig');
+
+module.exports = async function (context, deploymentStatusMessage) {
+  const Subscription = require('../models/Subscription');
+  context.log(
+    'JavaScript ServiceBus queue trigger function processed message',
+    deploymentStatusMessage
+  );
+  context.log(`Subscription Status: ${deploymentStatusMessage.status}`);
+  context.log(`Incoming Subscription ID: ${deploymentStatusMessage.subscriptionId}`);
+
+  try {
+    const subscription = await Subscription.findById(deploymentStatusMessage.subscriptionId);
+
+    subscription.status = deploymentStatusMessage.status;
+    await subscription.save();
+  } catch (err) {
+    context.log(`An error occured updating subscription status: ${err}`);
+  }
+};
