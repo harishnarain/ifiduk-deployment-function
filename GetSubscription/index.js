3560ac6548185c329933b1361ca6e00d5a162fad
Harish Narain <harishnarain@gmail.com>
2021-02-11 13:50:55 +1100

    Updated GetSubscriptions to only get subscriptions that are owned by org.

--- GetSubscription/index.js
+++ GetSubscription/index.js
@@ -2,6 +2,7 @@ const db = require('../config/dbConfig');
 
 module.exports = async function (context, req) {
   const Subscription = require('../models/Subscription');
+  const Organization = require('../models/Organization');
   db();
 
   context.log('Getting Subscriptions...');
@@ -10,26 +11,45 @@ module.exports = async function (context, req) {
   let query = '';
 
   if (req.query.name) {
-    query = new RegExp('^' + req.query.name, 'i');
+    //query = new RegExp('^' + req.query.name, 'i');
+    query = req.query.name.toLowerCase();
   }
 
-  try {
-    let subscriptions;
-    if (query) {
-      subscriptions = await Subscription.find({ name: { $regex: query } });
-    } else {
-      subscriptions = await Subscription.find();
-    }
-    context.log(subscriptions);
+  // Get subscriptions based on orgId
+  // Temporarily use static userIdentifier instead of claims for dev
+  const userIdentifier = '05d0ae08-ecb9-4054-9025-90410ac6f164';
 
-    return {
-      status: 200,
-      body: subscriptions,
-    };
+  try {
+    const orgSubscriptions = await Organization.findOne({ name: userIdentifier }).populate(
+      'subscriptions'
+    );
+
+    let subscriptions;
+
+    if (query && orgSubscriptions) {
+      filteredSubscriptions = orgSubscriptions.subscriptions.filter((subscription) =>
+        subscription.name.startsWith(query)
+      );
+      return {
+        status: 200,
+        body: filteredSubscriptions,
+      };
+    } else if (orgSubscriptions) {
+      return {
+        status: 200,
+        body: orgSubscriptions.subscriptions,
+      };
+    } else {
+      return {
+        status: 404,
+        body: 'No organization found!',
+      };
+    }
   } catch (err) {
+    context.log(err);
     return {
-      status: 400,
-      body: 'Error getting subscriptions!',
+      status: 500,
+      body: 'An error occured processing requests.',
     };
   }
 };
