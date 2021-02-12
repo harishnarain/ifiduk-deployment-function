d52387d67aa8e7262427cdcf62d1a0ce2dd91a4b
Harish Narain <harishnarain@gmail.com>
2021-02-12 16:41:53 +1100

    Refactored CreateSubscription to use MVC. Added authorization.

--- CreateSubscription/index.js
+++ CreateSubscription/index.js
@@ -1,173 +1,54 @@
-const db = require('../config/dbConfig');
-const validate = require('../shared/validate');
-const queueDeployment = require('../shared/queueDeployment');
-const { create } = require('../models/Subscription');
+const express = require('express');
+const createHandler = require('azure-function-express').createHandler;
+const passport = require('passport');
+const app = express();
+app.use(require('body-parser').urlencoded({ extended: true }));
 
-module.exports = async function (context, req) {
-  const Subscription = require('../models/Subscription');
-  const Product = require('../models/Product');
-  const Organization = require('../models/Organization');
-  db();
+const { createSubscription } = require('../controllers/SubscriptionController');
+const auth = require('../config/auth.json');
+const BearerStrategy = require('passport-azure-ad').BearerStrategy;
 
-  context.log('Creating Subscription...');
-  // Check to ensure a request body exists
-  if (!req.body) {
-    return {
-      status: 400,
-      body: 'A request body is required!',
-    };
-  }
-
-  // Extract oid from user claims
-  // Temporarily providing in request body for dev
-  const userIdentifier = '05d0ae08-ecb9-4054-9025-90410ac6f164';
-
-  // Extract org based on oid
-  let org;
-  try {
-    org = await Organization.findOne({ name: userIdentifier });
-  } catch (err) {
-    return {
-      status: 500,
-      body: 'An error occured processing your request.',
-    };
-  }
-
-  // Check if org exits
-  if (!org) {
-    return {
-      status: 400,
-      body: 'Error determining organization',
-    };
-  }
-
-  // Extract product ID from request body
-  let productId;
-  if (req.body.productId) {
-    productId = req.body.productId;
-  } else {
-    return {
-      status: 500,
-      body: 'Request body does not contain the productId!',
-    };
-  }
-
-  // Check if productId exists
-  try {
-    await Product.findById(productId);
-  } catch (err) {
-    return {
-      status: 400,
-      body: `An error occured finding product with ID: ${productId}`,
-    };
-  }
-
-  // Validate subscription name
-  let name;
-  if (req.body.name && validate.subscriptionName(req.body.name)) {
-    name = req.body.name.toLowerCase();
-  } else {
-    return {
-      status: 400,
-      body: `${req.body.name} is an invalid subscription name!`,
-    };
-  }
-
-  // Check if subscription name already exists
-  try {
-    const sub = await Subscription.findOne({ name: name });
-    if (sub) {
-      return {
-        status: 400,
-        body: `Subscription ${name} already exists!`,
-      };
-    }
-  } catch (err) {
-    return {
-      status: 400,
-      body: 'An error occured processing your request.',
-    };
-  }
-
-  // Create subscription
-  try {
-    const {
-      _id,
-      name: subName,
-      productId: subProductId,
-      orgId: subOrgId,
-      status: subStatus,
-    } = await Subscription.create({
-      name: name,
-      productId: productId,
-      orgId: org._id,
-      status: 'Pending',
-    });
-    await Organization.findByIdAndUpdate(org._id, { $push: { subscriptions: _id } }, { new: true });
-
-    // Prepare deployment message
-    const deploymentMessage = {
-      action: 'create',
-      name: name,
-      subscriptionId: _id,
-      frontend: {
-        name: `${_id}_fe`,
-      },
-      backend: {
-        name: `${_id}_be`,
-      },
-    };
-
-    // Create object for product
-    const product = await Product.findById(productId);
-
-    // Add container image names for frontend and backend
-    deploymentMessage.frontend.image = product.frontend.image;
-    deploymentMessage.backend.image = product.backend.image;
-
-    // Add ports to backend
-    deploymentMessage.backend.port = product.backend.port;
-
-    // Add environment variables to frontend and backend
-    if (product.frontend.env.length > 0) {
-      deploymentMessage.frontend.env = [];
-      product.frontend.env.forEach((env) => {
-        const key = Object.keys(env)[0];
-        let value;
-        if (env[key].includes('##BACKEND_HOST##')) {
-          value = env[key].replace('##BACKEND_HOST##', `${_id}_be`);
-        } else {
-          value = env[key];
-        }
-
-        const newEnv = {};
-        newEnv[key] = value;
-        deploymentMessage.frontend.env.push(newEnv);
-      });
-    }
-
-    if (product.backend.env.length > 0) {
-      deploymentMessage.backend.env = product.backend.env;
-    }
-
-    const message = await queueDeployment(deploymentMessage);
-
-    const createdSub = {
-      _id: _id,
-      name: subName,
-      productId: subProductId,
-      orgId: subOrgId,
-      status: subStatus,
-    };
-
-    return {
-      status: 201,
-      body: createdSub,
-    };
-  } catch (err) {
-    return {
-      status: 500,
-      body: `An error occured creating the subscriptions\n${err}`,
-    };
-  }
+const options = {
+  identityMetadata: `https://${auth.credentials.tenantName}.b2clogin.com/${auth.credentials.tenantName}.onmicrosoft.com/${auth.policies.policyName}/${auth.metadata.version}/${auth.metadata.discovery}`,
+  clientID: auth.credentials.clientID,
+  audience: auth.credentials.clientID,
+  policyName: auth.policies.policyName,
+  isB2C: auth.settings.isB2C,
+  validateIssuer: auth.settings.validateIssuer,
+  loggingLevel: auth.settings.loggingLevel,
+  passReqToCallback: auth.settings.passReqToCallback,
 };
+
+const bearerStrategy = new BearerStrategy(options, (token, done) => {
+  // Send user info using the second argument
+  done(null, {}, token);
+});
+
+app.use(passport.initialize());
+
+passport.use(bearerStrategy);
+
+//enable CORS (for testing only -remove in production/deployment)
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Authorization, Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+app.post(
+  '/api/subscriptions',
+  passport.authenticate('oauth-bearer', { session: false }),
+  async (req, res) => {
+    try {
+      const subscription = await createSubscription(req, res);
+      res.status(subscription.status).json(subscription.body);
+    } catch (err) {
+      res.status(err.status).json(err.body);
+    }
+  }
+);
+
+module.exports = createHandler(app);
--- controllers/SubscriptionController.js
+++ controllers/SubscriptionController.js
@@ -1,7 +1,8 @@
 const db = require('../config/dbConfig');
+const validate = require('../shared/validate');
+const queueDeployment = require('../shared/queueDeployment');
 
 exports.getSubscriptions = async (req, res) => {
-  const Subscription = require('../models/Subscription');
   const Organization = require('../models/Organization');
   db();
 
@@ -45,10 +46,178 @@ exports.getSubscriptions = async (req, res) => {
       };
     }
   } catch (err) {
-    context.log(err);
+    req.context.log(err);
     return {
       status: 500,
       body: 'An error occured processing requests.',
+    };
+  }
+};
+
+exports.createSubscription = async (req, res) => {
+  const Subscription = require('../models/Subscription');
+  const Product = require('../models/Product');
+  const Organization = require('../models/Organization');
+  db();
+
+  req.context.log('Creating Subscription...');
+  // Check to ensure a request body exists
+  if (!req.body) {
+    return {
+      status: 400,
+      body: 'A request body is required!',
+    };
+  }
+
+  // Extract oid from user claims
+  const userIdentifier = req.authInfo.oid;
+
+  // Extract org based on oid
+  let org;
+  try {
+    org = await Organization.findOne({ name: userIdentifier });
+  } catch (err) {
+    return {
+      status: 500,
+      body: 'An error occured processing your request.',
+    };
+  }
+
+  // Check if org exits
+  if (!org) {
+    return {
+      status: 400,
+      body: 'Error determining organization',
+    };
+  }
+
+  // Extract product ID from request body
+  let productId;
+  if (req.body.productId) {
+    productId = req.body.productId;
+  } else {
+    return {
+      status: 500,
+      body: 'Request body does not contain the productId!',
+    };
+  }
+
+  // Check if productId exists
+  try {
+    await Product.findById(productId);
+  } catch (err) {
+    return {
+      status: 400,
+      body: `An error occured finding product with ID: ${productId}`,
+    };
+  }
+
+  // Validate subscription name
+  let name;
+  if (req.body.name && validate.subscriptionName(req.body.name)) {
+    name = req.body.name.toLowerCase();
+  } else {
+    return {
+      status: 400,
+      body: `${req.body.name} is an invalid subscription name!`,
+    };
+  }
+
+  // Check if subscription name already exists
+  try {
+    const sub = await Subscription.findOne({ name: name });
+    if (sub) {
+      return {
+        status: 400,
+        body: `Subscription ${name} already exists!`,
+      };
+    }
+  } catch (err) {
+    return {
+      status: 400,
+      body: 'An error occured processing your request.',
+    };
+  }
+
+  // Create subscription
+  try {
+    const {
+      _id,
+      name: subName,
+      productId: subProductId,
+      orgId: subOrgId,
+      status: subStatus,
+    } = await Subscription.create({
+      name: name,
+      productId: productId,
+      orgId: org._id,
+      status: 'Pending',
+    });
+    await Organization.findByIdAndUpdate(org._id, { $push: { subscriptions: _id } }, { new: true });
+
+    // Prepare deployment message
+    const deploymentMessage = {
+      action: 'create',
+      name: name,
+      subscriptionId: _id,
+      frontend: {
+        name: `${_id}_fe`,
+      },
+      backend: {
+        name: `${_id}_be`,
+      },
+    };
+
+    // Create object for product
+    const product = await Product.findById(productId);
+
+    // Add container image names for frontend and backend
+    deploymentMessage.frontend.image = product.frontend.image;
+    deploymentMessage.backend.image = product.backend.image;
+
+    // Add ports to backend
+    deploymentMessage.backend.port = product.backend.port;
+
+    // Add environment variables to frontend and backend
+    if (product.frontend.env.length > 0) {
+      deploymentMessage.frontend.env = [];
+      product.frontend.env.forEach((env) => {
+        const key = Object.keys(env)[0];
+        let value;
+        if (env[key].includes('##BACKEND_HOST##')) {
+          value = env[key].replace('##BACKEND_HOST##', `${_id}_be`);
+        } else {
+          value = env[key];
+        }
+
+        const newEnv = {};
+        newEnv[key] = value;
+        deploymentMessage.frontend.env.push(newEnv);
+      });
+    }
+
+    if (product.backend.env.length > 0) {
+      deploymentMessage.backend.env = product.backend.env;
+    }
+
+    const message = await queueDeployment(deploymentMessage);
+
+    const createdSub = {
+      _id: _id,
+      name: subName,
+      productId: subProductId,
+      orgId: subOrgId,
+      status: subStatus,
+    };
+
+    return {
+      status: 201,
+      body: createdSub,
+    };
+  } catch (err) {
+    return {
+      status: 500,
+      body: `An error occured creating the subscriptions\n${err}`,
     };
   }
 };
