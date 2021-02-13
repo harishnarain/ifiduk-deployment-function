1330664859088b01996c32eb75a428e605dcf63a
Harish Narain <harishnarain@gmail.com>
2021-02-13 11:12:19 +1100

    Updated CreateOrganization to handle request from AAD B2C signup flow.

--- CreateOrganization/index.js
+++ CreateOrganization/index.js
@@ -4,18 +4,46 @@ module.exports = async function (context, req) {
   const Organization = require('../models/Organization');
   db();
 
+  const API_VERSION = '1.0.0';
+
+  // parse Basic Auth username and password
+  const header = req.headers['authorization'] || '', // get the header
+    token = header.split(/\s+/).pop() || '', // and the encoded auth token
+    auth = new Buffer.from(token, 'base64').toString(), // convert from base64
+    parts = auth.split(/:/), // split on colon
+    username = parts[0],
+    password = parts[1];
+
+  // Check for HTTP Basic Authentication, return HTTP 401 error if invalid credentials.
+  if (
+    username !== process.env['BASIC_AUTH_USERNAME'] ||
+    password !== process.env['BASIC_AUTH_PASSWORD']
+  ) {
+    context.res = {
+      status: 401,
+    };
+    context.log('Invalid Authentication');
+    return;
+  }
+
   context.log('Creating Organization...');
 
   // Check if organization name already exits
   // Extract oid from user claims
   // Temporarily providing in request body for dev
   if (!req.body) {
-    return {
+    context.res = {
       status: 400,
-      body: 'A request body is required!',
+      body: {
+        version: API_VERSION,
+        action: 'ShowBlockPage',
+        userMessage: 'A request body is required!',
+        status: 400,
+      },
     };
   }
-  const userIdentifier = '05d0ae08-ecb9-4054-9025-90410ac6f164';
+
+  const userIdentifier = req.body.email;
   const email = req.body.email.toLowerCase();
 
   // Find oid in organizations collections name field
@@ -23,9 +51,14 @@ module.exports = async function (context, req) {
   try {
     org = await Organization.findOne({ name: userIdentifier });
   } catch (err) {
-    return {
+    context.res = {
       status: 500,
-      body: 'An error occured processing requests.',
+      body: {
+        version: API_VERSION,
+        action: 'ShowBlockPage',
+        userMessage: 'An error occurred processing requests.',
+        status: 500,
+      },
     };
   }
 
@@ -38,24 +71,33 @@ module.exports = async function (context, req) {
     };
     try {
       const { _id, name, technicalContact } = await Organization.create(newOrg);
-      return {
+      context.res = {
         status: 201,
         body: {
-          _id: _id,
-          name: name,
-          technicalContact: technicalContact,
+          version: API_VERSION,
+          action: 'Continue',
         },
       };
     } catch (err) {
-      return {
+      context.res = {
         status: 500,
-        body: 'Error creating organization!',
+        body: {
+          version: API_VERSION,
+          action: 'ShowBlockPage',
+          userMessage: 'Error creating organization!',
+          status: 500,
+        },
       };
     }
   }
 
-  return {
-    status: 409,
-    body: 'Organization already exists!',
-  };
+  // context.res = {
+  //   status: 409,
+  //   body: {
+  //     version: API_VERSION,
+  //     action: 'ShowBlockPage',
+  //     userMessage: `Organization ${req.body.oid} already exists!`,
+  //     status: 409,
+  //   },
+  // };
 };
